156ae1212e2aaca5a0c968aeffac2058edfaa34f
hJ6129 <hywlss9@naver.com>
2018-11-21 16:30:32 +0900

    list3 li_head바꿈, 아이콘 ::before로 바꿈, FAQ 영역 js 수정

--- d2/list3/js/app.js
+++ d2/list3/js/app.js
@@ -4,10 +4,12 @@ $(document).ready(function(){
     
     function open(){
         $(this).siblings("p").toggleClass("answer");
+        $(this).css("font-weight") == "700" ? $(this).css({"font-weight":"400"}) : $(this).css({"font-weight":"bold"});
     }
 
     function close_all(){
         $("#faq_box>dl>dd>p").addClass("answer");
+        open_tag.css({"font-weight":"400"})
     }
 
     open_tag.on("click", open);
