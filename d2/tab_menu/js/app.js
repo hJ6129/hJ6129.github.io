cfc4b4895b40d7a5542ad51012ee00549ddd1d69
hJ6129 <hywlss9@naver.com>
2018-11-23 11:11:37 +0900

    tab_menu 완성

--- d2/tab_menu/js/app.js
+++ d2/tab_menu/js/app.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
 	$("#tab_menu>li").on("click",function(e){
-		if(event.target.children.length == "1"){
-			$(this).parent().toggleClass("select");
+		if(e.target.children.length == "1"){
+			$(this).parent().addClass("select");
 			$(this).toggleClass('focus');
 			$(this).find('ul').toggleClass('menu_on');
 			$(this).siblings().removeClass('focus');
 			$(this).siblings().find('ul').removeClass('menu_on');
+			if($(this).siblings().attr('class') != "focus" && $(this).attr('class') != "focus"){
+				$(this).parent().removeClass("select")
+				console.log("ASdf")
+			}
 		}
+
 	});
 });
