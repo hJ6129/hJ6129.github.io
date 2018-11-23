92da44cd315e9c4a91ec3f74e378070b0247f244
hJ6129 <hywlss9@naver.com>
2018-11-23 14:13:15 +0900

    코드 단축

--- d2/tab_menu/js/app.js
+++ d2/tab_menu/js/app.js
@@ -2,14 +2,10 @@ $(document).ready(function(){
 	$("#tab_menu>li").on("click",function(e){
 		if(e.target.children.length == "1"){
 			$(this).parent().addClass("select");
-			$(this).toggleClass('focus');
-			$(this).find('ul').toggleClass('menu_on');
-			$(this).siblings().removeClass('focus');
-			$(this).siblings().find('ul').removeClass('menu_on');
+			$(this).toggleClass('focus').siblings().removeClass('focus');
 			if($(this).siblings().attr('class') != "focus" && $(this).attr('class') != "focus"){
 				$(this).parent().removeClass("select")
 			}
 		}
-
 	});
 });
